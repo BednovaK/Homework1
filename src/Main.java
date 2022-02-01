c0d3ab356dea29dd4106c0af2f436ab82fcdc5e7
Bednova Ekaterina <96745291+BednovaK@users.noreply.github.com>
2022-02-01 23:16:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 2000;
+        int bonusMile = 20;
+
+        int bonus = priceTicket / bonusMile;
+
+
+        System.out.println("Начислено миль");
+        System.out.println(bonus);
+
+
+    }
+}
